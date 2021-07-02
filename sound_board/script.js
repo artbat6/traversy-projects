6bccb2e682f9644e0cfc88be3e36861f7a4cf6c0
artbat6 <arthurbatesdev@gmail.com>
2021-07-02 15:47:58 -0500

    renamed mp3's so their links would work

--- sound_board/script.js
+++ sound_board/script.js
@@ -1,4 +1,4 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach(sound => {
     const btn = document.createElement('button')
@@ -7,21 +7,19 @@ sounds.forEach(sound => {
     btn.innerText = sound
 
     btn.addEventListener('click', () => {
-       stopSongs()
-       
-        document.getElementById(sound).play() 
+        stopSongs()
+
+        document.getElementById(sound).play()
     })
 
-    document.getElementById('buttons').
-    appendChild(btn)
+    document.getElementById('buttons').appendChild(btn)
 })
 
 function stopSongs() {
     sounds.forEach(sound => {
-        const song = document.getElementById
-        (sound)
+        const song = document.getElementById(sound)
 
-        song.onpause()
-        song.currentTime = 0
+        song.pause()
+        song.currentTime = 0;
     })
 }
